0317b9324d6cc181a01064faefb20cef6bd80261
create <create@createsomething.xyz>
2026-01-21 14:53:01 -0600

    chore(outerfields): remove skip-to-content link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -41,11 +41,9 @@
 	});
 </script>
 
-<a href="#main-content" class="skip-link">Skip to main content</a>
-
 <div class="page-wrapper">
 	<Navigation user={data.user} />
-	<main id="main-content" class="main-wrapper" tabindex="-1">
+	<main class="main-wrapper">
 		{@render children()}
 	</main>
 	<Footer />
@@ -54,21 +52,6 @@
 <MiniPlayer />
 
 <style>
-	.skip-link {
-		position: absolute;
-		top: -100%;
-		left: 0;
-		padding: 0.5rem 1rem;
-		background: var(--color-primary);
-		color: var(--color-fg-primary);
-		z-index: 100;
-		border-radius: 0 0 0.25rem 0;
-	}
-
-	.skip-link:focus {
-		top: 0;
-	}
-
 	.page-wrapper {
 		min-height: 100vh;
 		display: flex;
